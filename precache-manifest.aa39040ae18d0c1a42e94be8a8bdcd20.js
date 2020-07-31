3ceecf447f2049d23875922c62a6704d57999053
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2020-07-31 09:10:18 +0700

    first commit

---
+++ precache-manifest.aa39040ae18d0c1a42e94be8a8bdcd20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892996306aad614d73aee8a08df2c85c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62305aabf54061ba0ee2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5e13ce2b2c83746b867a",
+    "url": "/static/css/main.69c4fd26.chunk.css"
+  },
+  {
+    "revision": "62305aabf54061ba0ee2",
+    "url": "/static/js/2.df3f712f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df3f712f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e13ce2b2c83746b867a",
+    "url": "/static/js/main.5144a2f1.chunk.js"
+  },
+  {
+    "revision": "319efb2bc1b3cbd6752d",
+    "url": "/static/js/runtime-main.e86234c4.js"
+  }
+]);
